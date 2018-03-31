9594331b13767a2509293a2b85de50bc1c7ddfea
andrew <kdk0063@gmail.com>
2018-03-31 00:35:44 -0700

    react temp updated

---
+++ InterviewMe.Web/Scripts/react-temp/src/components/AudioQuiz.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Navi from './Navbar';
+import '../css/audio.css'
+
+class AudioQuiz extends Compoent {
+
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+export default AudioQuiz;
---
+++ InterviewMe.Web/Scripts/react-temp/src/components/Quiz.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Navi from './Navbar';
+import '../css/quiz.css'
+
+class Quiz extends Compoent {
+
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+export default Quiz;
