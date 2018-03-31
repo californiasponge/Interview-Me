cb382a98a22492f194d0607b503adae9a82c0fdf
LTucker <lawrence.ed.tucker@gmail.com>
2018-03-31 11:15:21 -0700

    wip

--- InterviewMe.Web/Scripts/react-temp/src/components/Home.jsx
+++ InterviewMe.Web/Scripts/react-temp/src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import '../assets/css/main.css';
 import '../assets/css/home.css';
+import Ready from '../assets/img/AreYouReady.png';
 
 class Home extends Component {
     render() {
         return (
             <div className='row'>
+                <div className='ready'>
+                    <img src={Ready} alt='' />
+                    {/* <video className='ready-video' autoPlay loop muted>
+                        <source src={ReadyVideo} type='video/mp4' />
+                    </video> */}
+                </div>
                 <div className="match-stats card">
                     <h4 className="card-title">Leaderboard</h4>
                     <div className="card-body">
@@ -29,6 +36,7 @@ class Home extends Component {
                         </div>
                     </div>
                 </div>
+
             </div>
         )
     }
