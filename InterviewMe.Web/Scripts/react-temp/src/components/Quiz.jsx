9e9105774d47c1fd41f23b0259e6461c4353bded
andrew <kdk0063@gmail.com>
2018-03-31 18:44:51 -0700

    css

--- InterviewMe.Web/Scripts/react-temp/src/components/Quiz.jsx
+++ InterviewMe.Web/Scripts/react-temp/src/components/Quiz.jsx
@@ -13,12 +13,12 @@ class Quiz extends Component {
         return (
             <div className='quiz'>
                 <section className="section section-dark">
-                    <h2>Lets Get It!</h2>
+                    <h2 className='sectiontitle'>Lets Get It!</h2>
                     <div className='row'>                   
                         <div class="slideUp actions card col-xs-3">
                             <div class="card-body">
                                 <div className='icons'>
-                                <Ionicon icon="ios-add-circle-outline" fontSize="35px" color="#4257b2"/>
+                                <Ionicon icon="ios-add-circle-outline" fontSize="35px" color= "rgb(52, 128, 124)"/>
                                 </div>
                                 <a>Add New Questions</a>
                             </div>
@@ -26,7 +26,7 @@ class Quiz extends Component {
                         <div class="actions card col-xs-3">
                             <div class="card-body">
                                 <div className='icons'>
-                                <Ionicon icon="ios-card-outline" fontSize="35px" color="#4257b2"/>
+                                <Ionicon icon="ios-card-outline" fontSize="35px" color= "rgb(52, 128, 124)"/>
                                 </div>
                                 <a>Flash Cards</a>
                             </div>
@@ -34,7 +34,7 @@ class Quiz extends Component {
                         <div class="actions card col-xs-3">
                             <div class="card-body">
                                 <div className='icons'>
-                                <Ionicon icon="ios-paper-outline" fontSize="35px" color="#4257b2"/>
+                                <Ionicon icon="ios-paper-outline" fontSize="35px" color= "rgb(52, 128, 124)"/>
                                 </div>
                                 <a>Quiz</a>
                             </div>
@@ -42,7 +42,7 @@ class Quiz extends Component {
                         <div class="actions card col-xs-3">
                             <div class="card-body">
                                 <div className='icons'>
-                                <Ionicon icon="ios-microphone-outline" fontSize="35px" color="#4257b2"/>
+                                <Ionicon icon="ios-microphone-outline" fontSize="35px" color= "rgb(52, 128, 124)"/>
                                 </div>
                                 <a>Audio Test</a>
                             </div>
@@ -52,53 +52,53 @@ class Quiz extends Component {
 
                 <section className="section section-light">
                     <h2 className='sectionName'>Study Sets</h2>
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">C# / ASP.NET Fundamentals</h5>
-                            <p class="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classes and objects in C#, and types and assemblies.
+                    <div className="card col-lg 3">
+                        <div className="card-body">
+                            <h5 className="card-title">C# / ASP.NET Fundamentals</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
-                            <a href="#" class="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#4257b2"/></a>
-                            <a href="#" class="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#4257b2"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9"/></a>
                         </div>
                     </div>
 
-                     <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title"> Angular</h5>
-                            <p class="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classes and objects in C#, and types and assemblies.
+                    <div className="card col-lg 3">
+                        <div className="card-body">
+                            <h5 className="card-title"> Angular</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
-                            <a href="#" class="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#4257b2"/></a>
-                            <a href="#" class="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#4257b2"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9"/></a>
                         </div>
                     </div>
 
-                     <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">React</h5>
-                            <p class="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classes and objects in C#, and types and assemblies.
+                    <div className="card col-lg 3">
+                        <div className="card-body">
+                            <h5 className="card-title">React</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
-                            <a href="#" class="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#4257b2"/></a>
-                            <a href="#" class="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#4257b2"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9"/></a>
                         </div>
                     </div>
 
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">React Native</h5>
-                            <p class="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classes and objects in C#, and types and assemblies.
+                    <div className="card col-lg 3">
+                        <div className="card-body">
+                            <h5 className="card-title">React Native</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
-                            <a href="#" class="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#4257b2"/></a>
-                            <a href="#" class="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#4257b2"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9"/></a>
                         </div>
                     </div>
 
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">SQL Server 2016</h5>
-                            <p class="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classes and objects in C#, and types and assemblies.
+                    <div className="card col-lg 3">
+                        <div className="card-body">
+                            <h5 className="card-title">SQL Server 2016</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
-                            <a href="#" class="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#4257b2"/></a>
-                            <a href="#" class="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#4257b2"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9"/></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9"/></a>
                         </div>
                     </div>
 
