2f31c3207ca7b9ec00e6460077572d50535a1a2a
Karre Lawson <karrelawson@gmail.com>
2018-04-01 01:05:17 -0700

    wip

--- InterviewMe.Web/Scripts/react-temp/src/components/Speech.jsx
+++ InterviewMe.Web/Scripts/react-temp/src/components/Speech.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import * as SDK from 'microsoft-speech-browser-sdk';
 
 RecognizerSetup = (SDK, recognitionMode, language, format, subscriptionKey) => {
@@ -24,51 +24,51 @@ RecognizerStart = (SDK, recognizer) => {
             if (event instanceof SDK.RecognitionTriggeredEvent)
         */
         switch (event.Name) {
-            case "RecognitionTriggeredEvent" :
+            case "RecognitionTriggeredEvent":
                 UpdateStatus("Initializing");
                 break;
-            case "ListeningStartedEvent" :
+            case "ListeningStartedEvent":
                 UpdateStatus("Listening");
                 break;
-            case "RecognitionStartedEvent" :
+            case "RecognitionStartedEvent":
                 UpdateStatus("Listening_Recognizing");
                 break;
-            case "SpeechStartDetectedEvent" :
+            case "SpeechStartDetectedEvent":
                 UpdateStatus("Listening_DetectedSpeech_Recognizing");
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechHypothesisEvent" :
+            case "SpeechHypothesisEvent":
                 UpdateRecognizedHypothesis(event.Result.Text);
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechFragmentEvent" :
+            case "SpeechFragmentEvent":
                 UpdateRecognizedHypothesis(event.Result.Text);
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechEndDetectedEvent" :
+            case "SpeechEndDetectedEvent":
                 OnSpeechEndDetected();
                 UpdateStatus("Processing_Adding_Final_Touches");
                 console.log(JSON.stringify(event.Result)); // check console for other information in result
                 break;
-            case "SpeechSimplePhraseEvent" :
+            case "SpeechSimplePhraseEvent":
                 UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
                 break;
-            case "SpeechDetailedPhraseEvent" :
+            case "SpeechDetailedPhraseEvent":
                 UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
                 break;
-            case "RecognitionEndedEvent" :
+            case "RecognitionEndedEvent":
                 OnComplete();
                 UpdateStatus("Idle");
                 console.log(JSON.stringify(event)); // Debug information
                 break;
         }
     })
-    .On(() => {
-        // The request succeeded. Nothing to do here.
-    },
-    (error) => {
-        console.error(error);
-    });
+        .On(() => {
+            // The request succeeded. Nothing to do here.
+        },
+            (error) => {
+                console.error(error);
+            });
 }
 
 RecognizerStop = (SDK, recognizer) => {
@@ -76,3 +76,115 @@ RecognizerStop = (SDK, recognizer) => {
     recognizer.AudioSource.TurnOff();
 }
 
+export default class SpeechRec extends Component {
+    render() {
+        return (
+            <div id="content" style="display:none">
+                <table width="100%">
+
+                    <tr style="display:none">
+                        <td align="right">Subscription</td>
+                        <td><input id="key" type="text" size="40" value="486d780191184b088ee16d3f5e9395cd" /></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Language:</td>
+                        <td align="left">
+                            <select id="languageOptions">
+                                <option value="ar-EG">Arabic - EG</option>
+                                <option value="ca-ES">Catalan - ES</option>
+                                <option value="da-DK">Danish - DK</option>
+                                <option value="da-DK">Danish - DK</option>
+                                <option value="de-DE">German - DE</option>
+                                <option value="en-AU">English - AU</option>
+                                <option value="en-CA">English - CA</option>
+                                <option value="en-GB">English - GB</option>
+                                <option value="en-IN">English - IN</option>
+                                <option value="en-NZ">English - NZ</option>
+                                <option value="zh-CN">Chinese - CN</option>
+                                <option value="en-US" selected="selected">English - US</option>
+                                <option value="es-ES">Spanish - ES</option>
+                                <option value="es-MX">Spanish - MX</option>
+                                <option value="fi-FI">Finnish - FI</option>
+                                <option value="fr-CA">French - CA</option>
+                                <option value="fr-FR">French - FR</option>
+                                <option value="hi-IN">Hindi - IN</option>
+                                <option value="it-IT">Italian - IT</option>
+                                <option value="ja-JP">Japanese - JP</option>
+                                <option value="ko-KR">Korean - KR</option>
+                                <option value="nb-NO">Norwegian - NO</option>
+                                <option value="nl-NL">Dutch - NL</option>
+                                <option value="pl-PL">Polish - PL</option>
+                                <option value="pt-BR">Portuguese - BR</option>
+                                <option value="pt-PT">Portuguese - PT</option>
+                                <option value="ru-RU">Russian - RU</option>
+                                <option value="sv-SE">Swedish - SE</option>
+                                <option value="zh-CN">Chinese - CN</option>
+                                <option value="zh-HK">Chinese - HK</option>
+                                <option value="zh-TW">Chinese - TW</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr style="display:none">
+                        <td align="right">Recognition Mode:</td>
+                        <td align="left">
+                            <select id="recognitionMode">
+                                <option value="Interactive">Interactive</option>
+                                <option value="Conversation">Conversation</option>
+                                <option value="Dictation">Dictation</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr style="display:none">
+                        <td align="right">Format:</td>
+                        <td align="left">
+                            <select id="formatOptions">
+                                <option value="Simple" >Simple Result</option>
+                                <option selected="selected" value="Detailed">Detailed Result</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr style="display:none">
+                        <td align="right">Input:</td>
+                        <td align="left">
+                            <select id="inputSource">
+                                <option value="Mic" selected="selected">Microphone</option>
+                                <option value="File">Audio File</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <button id="startBtn" disabled="disabled">Start</button>
+                            <button id="stopBtn" disabled="disabled">Stop</button>
+                            <input type="file" id="filePicker" accept=".wav" style="display:none" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td style="display:none">Results:</td>
+                                    <td>Response:</td>
+                                    <td align="right">Status:</td>
+                                </tr>
+                                <tr>
+                                    <td style="display:none">
+                                        <textarea id="phraseDiv" style="display: inline-block;width:500px;height:200px"></textarea>
+                                    </td>
+                                    <td style="vertical-align: top">
+                                        <span id="hypothesisDiv" style="width:200px;height:200px;display:block;"></span>
+                                    </td>
+                                    <td align="left"><span id="statusDiv"></span></td>
+                                </tr>
+
+                            </table>
+                        </td>
+                    </tr>
+
+                </table>
+            </div>
+        )
+    }
+}
--- InterviewMe.Web/Scripts/react-temp/src/services/services.jsx
+++ InterviewMe.Web/Scripts/react-temp/src/services/services.jsx
@@ -9,9 +9,15 @@ export function getAll(){
 export function speech() {
     return axios({
         method: 'post',
-        url: '',
+        url: 'https://api.cognitive.microsoft.com/sts/v1.0',
         headers: {
-
+            "POST": "https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-US&format=detailed HTTP/1.1",
+            "Accept": "application/json;text/xml",
+            "Content-Type": "audio/wav; codec=audio/pcm; samplerate=16000",
+            "Ocp-Apim-Subscription-Key": "486d780191184b088ee16d3f5e9395cd",
+            "Host": "speech.platform.bing.com",
+            "Transfer-Encoding": "chunked",
+            "Expect": "100-continue"
         }
     })
 }
