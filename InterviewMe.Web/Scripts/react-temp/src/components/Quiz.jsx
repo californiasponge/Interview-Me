74970d43a3df09e7053ae467b682d461f53b005b
LTucker <lawrence.ed.tucker@gmail.com>
2018-03-31 21:54:17 -0700

    wip

--- InterviewMe.Web/Scripts/react-temp/src/components/Quiz.jsx
+++ InterviewMe.Web/Scripts/react-temp/src/components/Quiz.jsx
@@ -12,6 +12,7 @@ class Quiz extends Component {
     }
 
     getQuizzes = (subject) => {
+        console.log(subject);
         QuizService.getSubject(subject).then(
             response => console.log(response),
             error => alert('error')
@@ -60,9 +61,10 @@ class Quiz extends Component {
 
                 <section className="section section-light">
                     <h2 className='sectionName'>Study Sets</h2>
-                    <div className="card col-lg 3" onClick={() => this.getQuizzes('aspnet')}>
+
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('C#')}>
                         <div className="card-body">
-                            <h5 className="card-title">C# / ASP.NET Fundamentals</h5>
+                            <h5 className="card-title">C#</h5>
                             <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
                             <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
@@ -70,9 +72,9 @@ class Quiz extends Component {
                         </div>
                     </div>
 
-                    <div className="card col-lg 3">
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('Angular')}>
                         <div className="card-body">
-                            <h5 className="card-title"> Angular</h5>
+                            <h5 className="card-title">Angular</h5>
                             <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
                             </p>
                             <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
@@ -80,7 +82,17 @@ class Quiz extends Component {
                         </div>
                     </div>
 
-                    <div className="card col-lg 3">
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('ASP.NET')}>
+                        <div className="card-body">
+                            <h5 className="card-title">ASP.NET Fundamentals</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
+
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('React')}>
                         <div className="card-body">
                             <h5 className="card-title">React</h5>
                             <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
@@ -90,17 +102,7 @@ class Quiz extends Component {
                         </div>
                     </div>
 
-                    <div className="card col-lg 3">
-                        <div className="card-body">
-                            <h5 className="card-title">React Native</h5>
-                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
-                            </p>
-                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
-                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
-                        </div>
-                    </div>
-
-                    <div className="card col-lg 3">
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('SQL Server')}>
                         <div className="card-body">
                             <h5 className="card-title">SQL Server 2016</h5>
                             <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
@@ -109,7 +111,60 @@ class Quiz extends Component {
                             <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
                         </div>
                     </div>
-
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('JavaScript')}>
+                        <div className="card-body">
+                            <h5 className="card-title">JavaScript</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('OOP')}>
+                        <div className="card-body">
+                            <h5 className="card-title">OOP</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('Data Structures')}>
+                        <div className="card-body">
+                            <h5 className="card-title">Data Structures</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('ADO.NET')}>
+                        <div className="card-body">
+                            <h5 className="card-title">ADO.NET</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('HTML')}>
+                        <div className="card-body">
+                            <h5 className="card-title">HTML</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
+                    <div className="card col-lg 3" onClick={() => this.getQuizzes('Ajax')}>
+                        <div className="card-body">
+                            <h5 className="card-title">Ajax</h5>
+                            <p className="card-text">C# Fundamentals with C# 5.0. It’s designed to help you get everything you need to become a productive C# developer on the .NET platform, and covers things like the syntax of C#, which is similar to Java, JavaScript, and C++, classNamees and objects in C#, and types and assemblies.
+                            </p>
+                            <a href="#" className="card-link"><Ionicon icon="ios-star-outline" fontSize="20px" color="#3AAFA9" /></a>
+                            <a href="#" className="card-link"><Ionicon icon="md-volume-up" fontSize="20px" color="#3AAFA9" /></a>
+                        </div>
+                    </div>
                 </section>
 
             </div>
